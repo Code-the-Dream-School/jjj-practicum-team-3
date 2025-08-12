55b101439b5f13499668357a2ca6b78d995e28b9
Hugo  Calderon Delgado <hugocalderon@Mac.lan>
2025-08-12 14:21:07 -0700

    Add status codes to response

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -1,6 +1,7 @@
 export async function GET() {
   return new Response(JSON.stringify({ message: "Get All Movies" }), {
     headers: { "Content-Type": "application/json" },
+    status: 200,
   });
 }
 
@@ -9,21 +10,5 @@ export async function POST(request: Request) {
   return new Response(JSON.stringify("Movie created: " + body), {
     headers: { "Content-Type": "application/json" },
     status: 201,
-  });
-}
-
-export async function PATCH(request: Request) {
-  const body = await request.json();
-  return new Response(JSON.stringify("Movie modified: " + body), {
-    headers: { "Content-Type": "application/json" },
-    status: 204,
-  });
-}
-
-export async function DELETE(request: Request) {
-  const body = await request.json();
-  return new Response(JSON.stringify({message: "Movie Deleted"}), {
-    headers: { "Content-Type": "application/json" },
-    status: 204,
   });
 }
--- src/app/api/showtimes/route.ts
+++ src/app/api/showtimes/route.ts
@@ -1,6 +1,7 @@
 export async function GET() {
   return new Response(JSON.stringify({ message: "Get All Showtimes" }), {
     headers: { "Content-Type": "application/json" },
+    status: 200,
   });
 }
 
@@ -9,21 +10,5 @@ export async function POST(request: Request) {
   return new Response(JSON.stringify("Showtime created: " + body), {
     headers: { "Content-Type": "application/json" },
     status: 201,
-  });
-}
-
-export async function PATCH(request: Request) {
-  const body = await request.json();
-  return new Response(JSON.stringify("Showtime modified: " + body), {
-    headers: { "Content-Type": "application/json" },
-    status: 204,
-  });
-}
-
-export async function DELETE(request: Request) {
-  const body = await request.json();
-  return new Response(JSON.stringify({message: "Showtime Deleted"}), {
-    headers: { "Content-Type": "application/json" },
-    status: 204,
   });
 }
